60a7efe4f02b745271d9ba48baaedf5612b6d1f4
rakesh pawar <pawar2922@gmail.com>
2025-02-09 11:23:01 +0530

    made deployment ready

--- src/main/java/com/project/expenseTracker/config/CorsConfig.java
+++ src/main/java/com/project/expenseTracker/config/CorsConfig.java
@@ -3,6 +3,7 @@ package com.project.expenseTracker.config;
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
